49e86c05f925f8c8d1a97a7225cf82e0fa9c284a
keliang5 <159198151+keliang5@users.noreply.github.com>
2024-05-07 14:18:14 -0700

    L9 complete

--- M06.looting_workbook/assets/torch/torch.gd
+++ M06.looting_workbook/assets/torch/torch.gd
@@ -1,7 +1,19 @@
-extends Node2D
+extends Area2D
 
 @onready var flame: Sprite2D = $Flame
+
+func toggle_flame_visibility() -> void:
+	flame.visible = not flame.visible
 
 func _ready() -> void:
 	# This parameter of the shader material gives each flame a slightly different look and randomized animation.
 	flame.material.set("shader_parameter/offset", global_position * 0.1)
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+		)
+	if event_is_mouse_click:
+		toggle_flame_visibility()
--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -28,10 +28,11 @@ func _input_event(viewport: Node, event: InputEvent, shape_index: int):
 	var event_is_mouse_click: bool = (
 		event is InputEventMouseButton and
 		event.button_index == MOUSE_BUTTON_LEFT and
-		event.is_pressed()
+		event.is_pressed()	
 	)
 	if event_is_mouse_click:
 		open()
+		
 		
 func open() -> void:
 	animation_player.play("open")
