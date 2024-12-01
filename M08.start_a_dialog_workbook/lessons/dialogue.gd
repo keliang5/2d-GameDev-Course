3e28bc59cba6f119edd15786ba1aafa57b900334
keliang5 <159198151+keliang5@users.noreply.github.com>
2024-11-30 17:38:43 -0800

    M8 L7 Complete

--- M08.start_a_dialog_workbook/lessons/dialogue.gd
+++ M08.start_a_dialog_workbook/lessons/dialogue.gd
@@ -1,3 +1,4 @@
+@tool
 @icon("res://assets/dialogue_scene_icon.svg")
 extends Control
 
@@ -7,7 +8,8 @@ extends Control
 ## - expression: a [code]Texture[/code] containing an expression
 ## - text: a [code]String[/code] containing the text the character says
 ## - character: a [code]Texture[/code] representing the character
-@export var dialogue_items: Array[DialogueItem] = []
+@export var dialogue_items: Array[DialogueItem] = []:
+	set = set_dialogue_items
 
 
 ## UI element that shows the texts
@@ -23,6 +25,8 @@ extends Control
 
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
 	show_text(0)
 
 
@@ -92,3 +96,15 @@ func slide_in() -> void:
 	slide_tween.tween_property(body, "position:x", 0, 0.3)
 	body.modulate.a = 0
 	slide_tween.parallel().tween_property(body, "modulate:a", 1, 0.2)
+	
+func set_dialogue_items(new_dialogue_items: Array[DialogueItem]) -> void:
+	for index in new_dialogue_items.size():
+			if new_dialogue_items[index] == null:
+				new_dialogue_items[index] = DialogueItem.new()
+	dialogue_items = new_dialogue_items
+	update_configuration_warnings()
+	
+func _get_configuration_warnings() -> PackedStringArray:
+	if dialogue_items.is_empty():
+		return["You need at least one dialogue item for the dialogue system to work."]
+	return []
--- M08.start_a_dialog_workbook/lessons/dialogue_item.gd
+++ M08.start_a_dialog_workbook/lessons/dialogue_item.gd
@@ -1,5 +1,13 @@
+@tool
 @icon("res://assets/dialogue_item_icon.svg")
 class_name DialogueItem extends SlideShowEntry
 
+@export_group("Choices")
+@export var choices: Array[DialogueChoice] = []: set = set_dialogue_choices
 
-@export var choices: Array[DialogueChoice] = []
+
+func set_dialogue_choices(new_dialogue_choices: Array[DialogueChoice]) -> void:
+	for index in new_dialogue_choices.size():
+		if new_dialogue_choices[index] == null:
+			new_dialogue_choices[index] = DialogueChoice.new()
+	choices = new_dialogue_choices
